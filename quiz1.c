178a6cade93bd97935853b24bc7b356300f87c7a
mohamed-hs <mohamed_saleh@edu.aua.am>
2023-10-10 10:21:32 +0400

    Quiz trial, and lecture notes for 10/10

---
+++ quiz1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    int n = 0;
+    printf("Dear user, enter a larger number as a size of your number, say: 100\n ----> ");
+    scanf("%d", &n);
+    char number[n];
+    printf("SO which number is bothering you??\n");
+    scanf("%s", number);
+    printf("Oh...ohy my...um.. please wait :')\n");
+    return 0;
+}
+
+void add_one(char number[]){
+
+     for(int i = 0; i < )
+
+
+}
